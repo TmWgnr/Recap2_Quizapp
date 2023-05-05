04a9604e30c270691ac737a0a5853f325d99fb1b
Tim Wagner <tim.w@gner-mail.de>
2023-05-05 16:01:49 +0200

    add JS&form.html

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// phew… not a lot going on here. Please add some code!
+const bookmark = document.querySelector('[data-js="bookmark-button"]');
+bookmark.addEventListener("click", () => {
+  bookmark.classList.toggle("bookmark--active");
+});
+
+const answerButton = document.querySelector('[data-js="answer-button"]');
+
+const toggleAnswerButton = document.querySelector(
+  '[data-js="card__answer--toggle"]'
+);
+
+answerButton.addEventListener("click", () => {
+  toggleAnswerButton.classList.toggle("card__answer--active");
+});
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+});
